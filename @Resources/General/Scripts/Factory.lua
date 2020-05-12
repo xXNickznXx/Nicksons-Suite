d50ba4ebae6e56028dafd9b09abc130acc88fc04
Nickson <20nickw00@gmail.com>
2020-05-12 15:21:43 +0200

    Version 1.0.2

--- @Resources/General/Scripts/Factory.lua
+++ @Resources/General/Scripts/Factory.lua
@@ -1,34 +1,32 @@
 -- Structure of Script Measure:
 ---- IncFile=
+---- (Optional) Index=
 ---- Number=
 ---- SectionName=
 ---- OptionN=
 ---- ValueN=
----- where N is an ordered number from 0
--- Use %% to substitute it as the iteration number (which is specified by the Number option)
----- For example, if you specify 10, it will create 10 sections and replace the first section's %%
----- with 0, the second section's %% with 1, etc...
--- Wrap any formulas you want to parse in {} that otherwise RM would treat as a string
----- For example, [Measure{%%+1}] will have this script parse it for you
-
 function Initialize()
-	local num = SELF:GetNumberOption("Number")
-	local sectionName = SELF:GetOption("SectionName")
+	local index = SELF:GetNumberOption("Index")
+	local number = SELF:GetNumberOption("Number")
 
 	local file = io.open(SKIN:MakePathAbsolute(SELF:GetOption("IncFile")), "w")
 	
 	local t = {}
 	
-	for i = 0, num-1 do
-		table.insert(t, "[" .. doSub(sectionName, i) .. "]")
+	for i = index, number+index-1 do
+		table.insert(t, "[" .. doSub(SELF:GetOption("SectionName"), i) .. "]")
 		local j = 0
 		
 		while true do
-			local opt = SELF:GetOption("Option" .. j)
-			if opt == "" then
+			local option = SELF:GetOption("Option" .. j)
+			local value = SELF:GetOption("Value" .. j)
+			if option == "" then
 				break
+			elseif option == "Formula" then
+				table.insert(t, option .. "=" .. doSubSmooth(value, i, number))
+			else
+				table.insert(t, option .. "=" .. doSub(value, i))
 			end
-			table.insert(t, opt .. "=" .. doSub(SELF:GetOption("Value" .. j), i))
 			j = j + 1
 		end
 		
@@ -39,13 +37,50 @@ function Initialize()
 	file:close()
 end
 
--- does all the substitution!
+-- first if:
+-- SomeText%% -> SomeText0, SomeText1, ...
+-- second if:
+-- [Start=5,Inc=10] -> 5, 15, 25, ...
+-- third if:
+-- before[]<SomeText>after -> before[SomeText]after
+-- elseif:
+-- beforeSomeText[]after -> [beforeSomeTextafter]
 function doSub(value, i)
-	local val = value:gsub("%%%%", i)
-	if string.find(val, "%[%]") then
+	local val = value
+	if string.match(val, "%%%%") then
+		val = val:gsub("%%%%", i)
+	end
+	if string.match(val, "%[Start=%d+,Inc=%d+%]") then
+		local start = tonumber(string.match(val, "Start=(%d+)"))
+		local inc = tonumber(string.match(val, "Inc=(%d+)"))
+		val = start + inc * i
+	end
+	if string.match(val, "%[%]%b<>") then
+		val = val:gsub("%[%]", "")
+		val = val:gsub("<", "[")
+		val = val:gsub(">", "]")
+	elseif string.match(val, "%[%]") then
 		val = "[" .. val:gsub("%[%]", "") .. "]"
 	end
 	return val
+end
+
+function doSubSmooth(value, i, number)
+	local x = value
+	if i == 0 then
+		x = x:gsub("%%%%%-1", i)
+	else
+		x = x:gsub("%%%%%-1", i-1)
+	end
+	
+	if i == number-1 then
+		x = x:gsub("%%%%%+1", i)
+	else
+		x = x:gsub("%%%%%+1", i+1)
+	end
+	
+	x = x:gsub("%%%%", i)
+	return x
 end
 
 -- sub to remove {the curly braces}, then add (parentheses), then parse it
--- README.md
+++ README.md
@@ -1,36 +1,57 @@
 # Rainmeter
-Link to [DeviantArt](https://www.deviantart.com/xxnicksonxx/art/Nickson-Rainmeter-Skinpack-814984078).
+Link to [DeviantArt](https://www.deviantart.com/xxnicksonxx/art/Nicksons-Suite-814984078).
 
 
 # Add Languages:
-Create Pullrequest to add Language File in Folder: @Resources/General/Languages/
+Create Pullrequests for changes like:
+* adding a language in Folder: @Resources/General/Languages/
+* adding a cutout in folder:  @Resources/Cutout/
 
 
 # Skins
- * Clock
- * CPU
- * Cutout
- * Drives
- * GPU
- * Network (In & Out)
- * Player (Web)
- * RAM
- * Recycle Bin
- * Search (Google, Youtube, Stackoverflow)
- * Settings
- * Taskbar
- * TimeNDate
- * Translucent Taskbar
- * Visualizer
- * Weather
+* Clock
+* CPU
+* Cutout
+* Dark Contextmenu
+* Drives
+* GPU
+* Network (In & Out)
+* Player (Web)
+* RAM
+* Recycle Bin
+* Search (Google, Youtube, Stackoverflow)
+* Settings
+* Taskbar
+* TimeNDate
+* Translucent Taskbar
+* Visualizer
+* Weather
 
 
-# Change Log
+# Changelog ---------------
 ## 1.0.0
 ### Initial Release
 
 ## 1.0.1
-### Features
+### New
 * Added localization
-  * New Setting in Settings skin
-  * 2 languages: De, En
+  * New option in settings skin
+  * 2 initial languages: De, En
+
+## 1.0.2
+### New
+* Added translations for the context menu
+* Added DarkContextMenu plugin & skin
+* Added option to set dynamic number of drives
+### Changed
+* Default language is now "En" (English)
+* Change visualizer minheight from % to px
+* Sone design tweaks
+* In the settings, replaced some inputfields with sliders
+### Fixed
+* Fixed bug on player, when current time was 0:00 it doesnt showed up or when time was ending with 0:00 it got cut off
+* Fixed weather //Credits @jsmorley
+### Technical stuff
+* Rewrote "Factory.lua" (Merged Smooth&CustomIndex into it)
+* Added "Generator.lua" to dynamically generate skins
+* Rearranged Code for better readability
